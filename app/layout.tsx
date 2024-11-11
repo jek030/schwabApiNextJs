fae78102c76550fdbc3d57844bc58e291f3c09a4
James <jameskelly99999@gmail.com>
2024-11-10 22:09:36 -0500

    used cursor composer to make children full widths

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} h-screen flex flex-col divide-y divide-slate-300 overflow-auto`}>
         <div className="flex flex-row grow h-auto bg-gradient-to-br from-green-200 via-white to-green-100">
             <SideNav />           
-          <main className="h-full md:overflow-auto">{children}</main>
+          <main className="flex-1 w-full">{children}</main>
         </div>
         <div className="flex-none h-14">
           <Footer  />      
--- app/search/[ticker]/page.tsx
+++ app/search/[ticker]/page.tsx
@@ -46,107 +46,104 @@ export default async function Page({params} : {params: {ticker: string }}) {
     //todo : web service call to schwabapi
   const yahooURL = "https://finance.yahoo.com/quote/" + ticker
   return (
-    
-    <div className="flex flex-col p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-      <header className="flex flex-col  sm:items-start"> 
-        <p className=" text-gray-800 md:text-2xl md:leading-normal">
-           <strong >Welcome to FinanceGuy.</strong> This is the ticker page.
+    <div className="flex flex-col w-full gap-6 p-4">
+      <header className="flex flex-col sm:items-start px-4">
+        <p className="text-gray-800 md:text-2xl md:leading-normal">
+           <strong>Welcome to FinanceGuy.</strong> This is the ticker page.
         </p>  
         <Link
-              href=".."
-              className="border border-slate-200 mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-              >
-              Go Back 
-              </Link>
+          href=".."
+          className="border border-slate-200 mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        >
+          Go Back 
+        </Link>
       </header>
       
-      <main className="grid grid-cols-2 gap-8  sm:items-start">
+      <main className="grid grid-cols-1 lg:grid-cols-2 gap-6 px-4 w-full">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>{ticker}</CardTitle>       
+            <Divider></Divider>
+            <CardDescription>
+              { tickerData[ticker]?.reference?.description}
+            </CardDescription>
+          </CardHeader>
+          <CardContent >
+            Price: {formatter.format(tickerData[ticker]?.quote?.mark) }   <br></br>    
+            Net Change: <span style={{ color: getColor(tickerData[ticker]?.quote?.netChange)}}> {formatter.format(tickerData[ticker]?.quote?.netChange)} </span>          
+          </CardContent>
+          <CardContent>
+            <Link className="rounded-md bg-purple-500 px-4 py-2 text-sm text-white transition-colors hover:bg-purple-400"
+             href={yahooURL} 
+             target="_blank" 
+             rel="noopener noreferrer">{"Yahoo Finance"}
+            </Link>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>{ticker}</CardTitle>       
-          <Divider></Divider>
-          <CardDescription>
-            { tickerData[ticker]?.reference?.description}
-          </CardDescription>
-        </CardHeader>
-        <CardContent >
-          Price: {formatter.format(tickerData[ticker]?.quote?.mark) }   <br></br>    
-          Net Change: <span style={{ color: getColor(tickerData[ticker]?.quote?.netChange)}}> {formatter.format(tickerData[ticker]?.quote?.netChange)} </span>          
-        </CardContent>
-        <CardContent>
-          <Link className="rounded-md bg-purple-500 px-4 py-2 text-sm text-white transition-colors hover:bg-purple-400"
-           href={yahooURL} 
-           target="_blank" 
-           rel="noopener noreferrer">{"Yahoo Finance"}
-          </Link>
-        </CardContent>
-      </Card>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Statistics</CardTitle>       
+            <Divider></Divider>       
+          </CardHeader>
+          <CardContent>
+              52 week high: {tickerData[ticker]?.quote["52WeekHigh"]} <br></br>
+              52 week low: {tickerData[ticker]?.quote["52WeekLow"]} <br></br>
+              10 day average volume: {formatterVol.format(tickerData[ticker]?.fundamental?.avg10DaysVolume)} <br></br>      
+              1 year average volume: { formatterVol.format(tickerData[ticker]?.fundamental?.avg1YearVolume)} <br></br>  
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Statistics</CardTitle>       
-          <Divider></Divider>       
-        </CardHeader>
-        <CardContent>
-            52 week high: {tickerData[ticker]?.quote["52WeekHigh"]} <br></br>
-            52 week low: {tickerData[ticker]?.quote["52WeekLow"]} <br></br>
-            10 day average volume: {formatterVol.format(tickerData[ticker]?.fundamental?.avg10DaysVolume)} <br></br>      
-            1 year average volume: { formatterVol.format(tickerData[ticker]?.fundamental?.avg1YearVolume)} <br></br>  
-        </CardContent>
-      </Card>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Fundamentals</CardTitle>       
+            <Divider></Divider>       
+          </CardHeader>
+          <CardContent>
+              P/E Ratio:<span style={{ color: getColor(tickerData[ticker]?.fundamental?.peRatio)}}> {formatter.format(tickerData[ticker]?.fundamental?.peRatio)} </span> <br></br>
+              EPS:<span style={{ color: getColor(tickerData[ticker]?.fundamental?.eps)}}> {formatter.format(tickerData[ticker]?.fundamental?.eps)} </span> <br></br>
+              Next ex-dividend date: {new Date(tickerData[ticker]?.fundamental?.nextDivExDate).toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}<br></br>
+              Next dividend payment date: {new Date(tickerData[ticker]?.fundamental?.nextDivPayDate).toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}<br></br>
+              Dividend yield: {formatter.format(tickerData[ticker]?.fundamental?.divYield)}%<br></br>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle>Fundamentals</CardTitle>       
-          <Divider></Divider>       
-        </CardHeader>
-        <CardContent>
-            P/E Ratio:<span style={{ color: getColor(tickerData[ticker]?.fundamental?.peRatio)}}> {formatter.format(tickerData[ticker]?.fundamental?.peRatio)} </span> <br></br>
-            EPS:<span style={{ color: getColor(tickerData[ticker]?.fundamental?.eps)}}> {formatter.format(tickerData[ticker]?.fundamental?.eps)} </span> <br></br>
-            Next ex-dividend date: {new Date(tickerData[ticker]?.fundamental?.nextDivExDate).toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}<br></br>
-            Next dividend payment date: {new Date(tickerData[ticker]?.fundamental?.nextDivPayDate).toLocaleString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})}<br></br>
-            Dividend yield: {formatter.format(tickerData[ticker]?.fundamental?.divYield)}%<br></br>
-        </CardContent>
-      </Card>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Daily</CardTitle>       
-          <Divider></Divider>       
-        </CardHeader>
-        <CardContent>
-            Regular market price {formatter.format(tickerData[ticker]?.regular?.regularMarketLastPrice)} <br></br>
-            Daily volume: {formatterVol.format(tickerData[ticker]?.quote?.totalVolume)} <br></br>
-            Regular market net change {formatter.format(tickerData[ticker]?.regular?.regularMarketNetChange)} <br></br>
-            Regular market % change {formatter.format(tickerData[ticker]?.regular?.regularMarketPercentChange)} <br></br><br></br>
-            Last close price {formatter.format(tickerData[ticker]?.quote?.closePrice)} <br></br>
-            Daily high: {formatter.format(tickerData[ticker]?.quote?.highPrice)} <br></br>
-            Daily low: {formatter.format(tickerData[ticker]?.quote?.lowPrice)} <br></br><br></br>
-            Mark: {formatter.format(tickerData[ticker]?.quote?.mark)} <br></br>
-            Daily % change: <span style={{ color: getColor(tickerData[ticker]?.quote?.netPercentChange)}}> {formatter.format(tickerData[ticker]?.quote?.netPercentChange)}% </span> <br></br>        
-            Daily net change: <span style={{ color: getColor(tickerData[ticker]?.quote?.netChange)}}> {formatter.format(tickerData[ticker]?.quote?.netChange)} </span> <br></br>
-            After hours change: <span style={{ color: getColor(tickerData[ticker]?.quote?.postMarketChange)}}> {formatter.format(tickerData[ticker]?.quote?.postMarketChange)} </span> <br></br>
-            After hours % change: <span style={{ color: getColor(tickerData[ticker]?.quote?.postMarketPercentChange)}}> {formatter.format(tickerData[ticker]?.quote?.postMarketPercentChange)} </span> <br></br>
-        </CardContent>
-      </Card>  
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Additional Data</CardTitle>
-          <CardDescription>
-            This section will contain additional information
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Suspense fallback={EmptyDataTableSkeleton(columns)}>
-            <div className="min-h-[200px] flex items-center justify-center text-gray-500">
-              Future content will be loaded here
-            </div>
-          </Suspense>
-        </CardContent>
-      </Card>   
-    </main>    
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Daily</CardTitle>       
+            <Divider></Divider>       
+          </CardHeader>
+          <CardContent>
+              Regular market price {formatter.format(tickerData[ticker]?.regular?.regularMarketLastPrice)} <br></br>
+              Daily volume: {formatterVol.format(tickerData[ticker]?.quote?.totalVolume)} <br></br>
+              Regular market net change {formatter.format(tickerData[ticker]?.regular?.regularMarketNetChange)} <br></br>
+              Regular market % change {formatter.format(tickerData[ticker]?.regular?.regularMarketPercentChange)} <br></br><br></br>
+              Last close price {formatter.format(tickerData[ticker]?.quote?.closePrice)} <br></br>
+              Daily high: {formatter.format(tickerData[ticker]?.quote?.highPrice)} <br></br>
+              Daily low: {formatter.format(tickerData[ticker]?.quote?.lowPrice)} <br></br><br></br>
+              Mark: {formatter.format(tickerData[ticker]?.quote?.mark)} <br></br>
+              Daily % change: <span style={{ color: getColor(tickerData[ticker]?.quote?.netPercentChange)}}> {formatter.format(tickerData[ticker]?.quote?.netPercentChange)}% </span> <br></br>        
+              Daily net change: <span style={{ color: getColor(tickerData[ticker]?.quote?.netChange)}}> {formatter.format(tickerData[ticker]?.quote?.netChange)} </span> <br></br>
+              After hours change: <span style={{ color: getColor(tickerData[ticker]?.quote?.postMarketChange)}}> {formatter.format(tickerData[ticker]?.quote?.postMarketChange)} </span> <br></br>
+              After hours % change: <span style={{ color: getColor(tickerData[ticker]?.quote?.postMarketPercentChange)}}> {formatter.format(tickerData[ticker]?.quote?.postMarketPercentChange)} </span> <br></br>
+          </CardContent>
+        </Card>  
+        <Card className="w-full lg:col-span-2">
+          <CardHeader>
+            <CardTitle>Additional Data</CardTitle>
+            <CardDescription>
+              This section will contain additional information
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Suspense fallback={EmptyDataTableSkeleton(columns)}>
+              <div className="min-h-[200px] flex items-center justify-center text-gray-500">
+                Future content will be loaded here
+              </div>
+            </Suspense>
+          </CardContent>
+        </Card>   
+      </main>    
     </div>
   );
 }
